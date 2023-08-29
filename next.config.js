470fa3ac3eabb47155ada383b53305f867b9db26
Md.Hasanat Zamil <zamil_surfserver@yahoo.com>
2023-08-30 01:36:12 +0600

    updateconfig note

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig}  */
 const nextConfig = {
   env: {
-    BASE_URL: "https://ostad-module-13.vercel.app/",
+    BASE_URL: "https://module-13-ten.vercel.app/",
   },
 };
 module.exports = nextConfig;
